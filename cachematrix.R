b3f24423af7fd4a3921466ede31fe32cd5637b93
MarcelvanDorsten <[info@asdonline.nl]>
2015-07-23 22:56:31 +0200

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes advantage of the scoping environment and reads from this 
+## cache and does his computation therefore faster especially by big files  
 
-## Write a short comment describing this function
+
+## Invert the variables and put them in the scoping environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+          x <<- y
+          m <<- NULL
+      }
+      get <- function() x
+      setmatrix <- function(matrix) m <<- matrix
+      getmatrix <- function() m
+      list(set = set, get = get, 
+           setmatrix = setmatrix,
+           getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Calculates the matrix of the above created makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
